6b592e77c362db7c93f97c2c69ea3899293a7b13
jeremyvernon1 <60850051+jeremyvernon1@users.noreply.github.com>
2020-06-18 07:41:09 -0400

    Initial commit

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Jeremy Vernon
+# Date: 18 Jun 2020
+# Description: Asks the user for their favorite animal and then prints out their favorite animal.
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
